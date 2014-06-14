2c83f14f07ca49c0c47e2c139a8d25ed27478172
mrb00l34n <rsabb24@gmail.com>
2014-06-13 23:45:09 -0400

    Average Case, Faster Calculations

--- variance_simulator.py
+++ variance_simulator.py
@@ -1,5 +1,8 @@
 from random import normalvariate
 import matplotlib.pyplot as plt
+from pylab import * 
+
+rcParams['figure.figsize'] = 50, 10
 
 def profit_per_hand(winrate, stdev):
 	winrate_per_hand = winrate/100.0
@@ -8,20 +11,23 @@ def profit_per_hand(winrate, stdev):
 
 def simulation(number_of_hands,runs,winrate,stdev):
 	results_per_hand = []
-	results_per_run = []
-
 	for run in range(runs):
-		results_per_hand.append([profit_per_hand(winrate,stdev) for hand in xrange(1,number_of_hands)])
+		results_per_hand.append([profit_per_hand(winrate,stdev) for hand in xrange(1,number_of_hands+1)])
 
 	for profit in results_per_hand:
-		results_per_run.append([sum(profit[:hand]) for hand in xrange(1,number_of_hands)])
+		plt.plot([sum(profit[:hand]) for hand in xrange(0,number_of_hands+1)])
 	
-	for run in results_per_run:
-		plt.plot(run)
-
-	plt.title('Variance Simulation')
-	plt.ylabel('Profit')
-	plt.xlabel('Number of Hands')
+	# Plot the cumulative average of each run
+	avg = [sum(col)/len(col) for col in zip(*results_per_hand)]
+	average, = plt.plot([sum(avg[:hand]) for hand in xrange(0,number_of_hands+1)],linewidth=4)
+	l1 = plt.legend([average], ["Average"], loc=1)
+	plt.gca().add_artist(l1)
+	
+	plt.axhline(linewidth=2)
+	plt.title('Simulating Results of Poker Player with %d bb/100 winrate and %d bb/100 standard deviation'\
+		% (winrate,stdev))
+	plt.ylabel('Profit (bb)')
+	plt.xlabel('Number of Hands') 
 	plt.show()
 
 number_of_hands = int(raw_input("How many hands do you want to simulate? "))
